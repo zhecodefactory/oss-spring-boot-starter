bbc69c2187393a1d5e9111453100f07f0cc3e57e
wangzh@zgjzd.cn <znFSwuQWdz73qpYe75eH>
2023-05-04 09:55:32 +0800

    基于AmazonS3实现的对象存储统一管理Start

---
+++ src/main/java/com/wz/ossspringbootstarter/configuration/OssAutoConfiguration.java
@@ -0,0 +1,56 @@
+package com.wz.ossspringbootstarter.configuration;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.wz.ossspringbootstarter.propieties.OssProperties;
+import com.wz.ossspringbootstarter.template.OssTemplate;
+import com.wz.ossspringbootstarter.template.impl.OssTemplateImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 王哲
+ * @ClassName OssAutoConfiguration
+ * @create 2023--五月--上午9:13
+ * @Description Oss的自动配置类
+ * @Version V1.0
+ */
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(OssProperties.class)
+public class OssAutoConfiguration {
+    @Bean
+    @ConditionalOnMissingBean
+    public AmazonS3 ossClient(OssProperties ossProperties) {
+        // 客户端配置，主要是全局的配置信息
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setMaxConnections(ossProperties.getMaxConnections());
+        // url以及region配置
+        AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(
+                ossProperties.getEndpoint(), ossProperties.getRegion());
+        // 凭证配置
+        AWSCredentials awsCredentials = new BasicAWSCredentials(ossProperties.getAccessKey(),
+                ossProperties.getSecretKey());
+        AWSCredentialsProvider awsCredentialsProvider = new AWSStaticCredentialsProvider(awsCredentials);
+        // build amazonS3Client客户端
+        return AmazonS3Client.builder().withEndpointConfiguration(endpointConfiguration)
+                .withClientConfiguration(clientConfiguration).withCredentials(awsCredentialsProvider)
+                .disableChunkedEncoding().withPathStyleAccessEnabled(ossProperties.getPathStyleAccess()).build();
+    }
+
+    @Bean
+    @ConditionalOnBean(AmazonS3.class)
+    public OssTemplate ossTemplate(AmazonS3 amazonS3){
+        return new OssTemplateImpl(amazonS3);
+    }
+}
---
+++ src/test/java/com/wz/ossspringbootstarter/OssSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wz.ossspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
